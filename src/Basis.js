c5f753911e276dce7572f6b61f359601a0fe47e5
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-04-21 13:38:36 +0800

    +Navbar

--- src/Basis.js
+++ src/Basis.js
@@ -8,13 +8,13 @@ class Basis extends Component {
         <div>
             <div className="ui pointing menu" id="nav">
                 <button className="ui circular icon button" role="button" id="circle-button">
-                    <i aria-hidden="true" className="settings icon"></i>
+                    <i aria-hidden="true" className="align justify icon"></i>
                 </button>
                 <h1 id="header-text"> moodal </h1>
+                <button className="ui circular icon button" role="button" id="circle-button">
+                    <i className="user circle icon"></i>
+                </button>
             </div>
-            {/* <div class="ui segment">
-                <img src="/assets/images/wireframe/media-paragraph.png" />
-            </div> */}
         </div>
     );
   }
